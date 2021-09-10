78416520dd9564f26bd1487fa8b0389d18526159
aicha laafia <54075891+alaafia@users.noreply.github.com>
2021-09-10 22:12:19 +0100

    spring initial

---
+++ GestionComptesBanque/src/test/java/com/Laafia/GestionComptesBanque/GestionComptesBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Laafia.GestionComptesBanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionComptesBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
