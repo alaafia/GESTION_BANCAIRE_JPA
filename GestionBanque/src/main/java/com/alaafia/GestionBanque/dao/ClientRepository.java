2b13eb6f2cf02ac5fad460391539f4e4a0977b00
aicha laafia <54075891+alaafia@users.noreply.github.com>
2021-09-13 22:12:26 +0100

    test

--- GestionBanque/src/main/java/com/alaafia/GestionBanque/dao/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alaafia.GestionBanque.dao;
-
-import com.alaafia.GestionBanque.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- GestionBanque/src/main/java/com/alaafia/GestionBanque/dao/CompteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alaafia.GestionBanque.dao;
-
-import com.alaafia.GestionBanque.entities.Compte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompteRepository extends JpaRepository<Compte, String> {
-
-}
